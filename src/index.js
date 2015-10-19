e996a349e0910f44c147dff3642d8fc1f3f39fff
Kirill Fomichev <fanatid@ya.ru>
2015-10-19 10:47:34 +0300

    remove bitcore

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-import bitcore from 'bitcore'
+import createHash from 'create-hash'
+import bs58check from 'bs58check'
 
-let Address = bitcore.Address
-let base58encode = bitcore.encoding.Base58Check.encode
-let Networks = bitcore.Networks
-let Opcode = bitcore.Opcode
-let Script = bitcore.Script
-let sha256ripemd160 = bitcore.crypto.Hash.sha256ripemd160
+import networks from './networks.json'
+import opcodes from './opcodes.json'
+
+/**
+ * @param {Buffer} buf
+ * @return {Buffer}
+ */
+function sha256ripemd160 (buf) {
+  buf = createHash('sha256').update(buf).digest()
+  return createHash('ripemd160').update(buf).digest()
+}
 
 /**
  * @param {number} version
@@ -15,7 +21,7 @@ let sha256ripemd160 = bitcore.crypto.Hash.sha256ripemd160
 function createAddress (version, hashBuffer) {
   let versionBuffer = new Buffer([version])
   let buffer = Buffer.concat([versionBuffer, hashBuffer])
-  return base58encode(buffer)
+  return bs58check.encode(buffer)
 }
 
 /**
@@ -24,10 +30,7 @@ function createAddress (version, hashBuffer) {
  * @return {boolean}
  */
 function isPublicKey (buffer, strict) {
-  if (!buffer) {
-    return false
-  }
-
+  // TODO: need more check
   switch (buffer[0]) {
     case 0x02:
     case 0x03:
@@ -43,98 +46,144 @@ function isPublicKey (buffer, strict) {
 }
 
 /**
- * @param {*} script
- * @param {*} [network=bitcore.Networks.defaultNetwork]
+ * @param {Buffer} buf
+ * @param {number} offset
+ * @param {boolean} strict
+ * @return {?{bytes: number, size: number}}
+ */
+function readDataSize (buf, offset, strict) {
+  let opcode = buf[offset]
+
+  if ((strict && opcode >= opcodes.OP_PUSHDATA1) ||
+      opcode > opcodes.OP_PUSHDATA4 ||
+      buf.length < offset + opcode - opcodes.OP_PUSHDATA1 + (opcode === opcodes.OP_PUSHDATA4 ? 3 : 2)) {
+    return null
+  }
+
+  switch (opcode) {
+    case opcodes.OP_PUSHDATA4:
+      return {bytes: 5, size: buf.readUInt32LE(offset + 1)}
+    case opcodes.OP_PUSHDATA2:
+      return {bytes: 3, size: buf.readUInt16LE(offset + 1)}
+    case opcodes.OP_PUSHDATA1:
+      return {bytes: 2, size: buf.readUInt8(offset + 1)}
+    default:
+      return {bytes: 1, size: opcode}
+  }
+}
+
+/**
+ * @param {(Buffer|string)} buf
+ * @param {({pubkeyhash: number, scripthash: number}|string)} [network={pubkeyhash: 0x80, scripthash: 0x05}]
  * @param {boolean} [strict=false]
  * @return {{type: string, addresses: Array.<string>}}
  */
-export default function (script, network, strict) {
-  try {
-    script = new Script(script)
-  } catch (err) {
-    return {type: 'unknow'}
+export default function (buf, network, strict) {
+  if (!Buffer.isBuffer(buf)) {
+    try {
+      buf = new Buffer(buf, 'hex')
+    } catch (err) {
+      return {type: 'unknow', addresses: []}
+    }
   }
 
-  let sChunks = script.chunks
-  if (sChunks.length === 0) {
-    return {type: 'unknow'}
+  if (Object.prototype.toString.call(network) === '[object String]') {
+    network = networks[network]
   }
 
-  network = network || Networks.get(network) || Networks.defaultNetwork
-  switch (sChunks[0].opcodenum) {
+  if (Object.prototype.toString.call(network) !== '[object Object]') {
+    network = networks.mainnet
+  }
+
+  let dataSize
+  switch (buf[0]) {
     // pubkeyhash
-    case Opcode.OP_DUP:
-      if (!(sChunks.length === 5 &&
-            sChunks[1].opcodenum === Opcode.OP_HASH160 &&
-            sChunks[2].buf &&
-            sChunks[2].buf.length === 20 &&
-            sChunks[3].opcodenum === Opcode.OP_EQUALVERIFY &&
-            sChunks[4].opcodenum === Opcode.OP_CHECKSIG) ||
-          (strict && sChunks[2].opcodenum !== 20)) {
-        return {type: 'unknow'}
+    case opcodes.OP_DUP:
+      if (buf.length < 25 ||
+          buf.length > (strict ? 25 : 29) ||
+          buf[1] !== opcodes.OP_HASH160 ||
+          buf[buf.length - 2] !== opcodes.OP_EQUALVERIFY ||
+          buf[buf.length - 1] !== opcodes.OP_CHECKSIG) {
+        break
       }
 
+      dataSize = readDataSize(buf, 2, strict)
+      if (dataSize === null || dataSize.size !== 20) {
+        break
+      }
+
+      buf = buf.slice(2 + dataSize.bytes, buf.length - 2)
       return {
         type: 'pubkeyhash',
-        addresses: [
-          createAddress(network[Address.PayToPublicKeyHash], sChunks[2].buf)
-        ]
+        addresses: [createAddress(network.pubkeyhash, buf)]
       }
 
     // scripthash
-    case Opcode.OP_HASH160:
-      if (!(sChunks.length === 3 &&
-            sChunks[1].buf &&
-            sChunks[1].buf.length === 20 &&
-            sChunks[2].opcodenum === Opcode.OP_EQUAL) ||
-          (strict && sChunks[1].opcodenum !== 20)) {
-        return {type: 'unknow'}
+    case opcodes.OP_HASH160:
+      if (buf.length < 23 ||
+          buf.length > (strict ? 23 : 27) ||
+          buf[buf.length - 1] !== opcodes.OP_EQUAL) {
+        break
       }
 
+      dataSize = readDataSize(buf, 1, strict)
+      if (dataSize === null || dataSize.size !== 20) {
+        break
+      }
+
+      buf = buf.slice(1 + dataSize.bytes, buf.length - 1)
       return {
         type: 'scripthash',
-        addresses: [
-          createAddress(network[Address.PayToScriptHash], sChunks[1].buf)
-        ]
+        addresses: [createAddress(network.scripthash, buf)]
       }
 
     // nulldata
-    case Opcode.OP_RETURN:
-      return {type: 'nulldata'}
+    case opcodes.OP_RETURN:
+      return {type: 'nulldata', addresses: []}
 
     // pubkey & multisig
     default:
       // pubkey
-      if ((sChunks.length === 2 &&
-           isPublicKey(sChunks[0].buf, strict) &&
-           sChunks[1].opcodenum === Opcode.OP_CHECKSIG) &&
-          !(strict &&
-            sChunks[0].opcodenum !== 33 &&
-            sChunks[0].opcodenum !== 65)) {
-        let hashBuffer = sha256ripemd160(sChunks[0].buf)
+      if (buf[buf.length - 1] === opcodes.OP_CHECKSIG) {
+        dataSize = readDataSize(buf, 0, strict)
+        if (dataSize === null) {
+          break
+        }
+
+        buf = buf.slice(dataSize.bytes, buf.length - 1)
+        if (!isPublicKey(buf, strict)) {
+          break
+        }
+
         return {
           type: 'pubkey',
-          addresses: [
-            createAddress(network[Address.PayToPublicKeyHash], hashBuffer)
-          ]
+          addresses: [createAddress(network.pubkeyhash, sha256ripemd160(buf))]
         }
       }
 
       // multisig
-      let mOp = sChunks[0].opcodenum
-      let nOp = sChunks[Math.max(sChunks.length - 2, 0)].opcodenum
-      if (sChunks.length >= 4 &&
-          sChunks[sChunks.length - 1].opcodenum === Opcode.OP_CHECKMULTISIG &&
-          mOp >= Opcode.OP_1 &&
-          mOp <= Opcode.OP_16 &&
-          nOp >= Opcode.OP_1 &&
-          nOp <= Opcode.OP_16 &&
-          nOp >= mOp &&
-          nOp - Opcode.OP_1 === sChunks.length - 4 &&
-          sChunks.slice(1, -2).every((o) => isPublicKey(o.buf, strict))) {
-        let addresses = sChunks.slice(1, -2).map((o) => {
-          let hashBuffer = sha256ripemd160(o.buf)
-          return createAddress(network[Address.PayToPublicKeyHash], hashBuffer)
+      let mOp = buf[0]
+      let nOp = buf[buf.length - 2]
+      let isMultisig = (buf[buf.length - 1] === opcodes.OP_CHECKMULTISIG &&
+                        mOp >= opcodes.OP_1 &&
+                        nOp <= opcodes.OP_16 &&
+                        nOp >= mOp)
+
+      let pubKeys = []
+      for (let offset = 1, stop = buf.length - 2; isMultisig && offset < stop;) {
+        dataSize = readDataSize(buf, offset, strict)
+        if (dataSize === null) {
+          isMultisig = false
+        } else {
+          pubKeys.push(buf.slice(offset + dataSize.bytes, offset + dataSize.bytes + dataSize.size))
+          isMultisig = isPublicKey(pubKeys[pubKeys.length - 1], strict)
+          offset += dataSize.bytes + dataSize.size
+        }
+      }
+
+      if (isMultisig && pubKeys.length === nOp - opcodes.OP_1 + 1) {
+        let addresses = pubKeys.map((pubKey) => {
+          return createAddress(network.pubkeyhash, sha256ripemd160(pubKey))
         })
         return {
           type: 'multisig',
@@ -143,8 +192,8 @@ export default function (script, network, strict) {
           })
         }
       }
-
-      // unknow output script type
-      return {type: 'unknow'}
   }
+
+  // unknow output script type
+  return {type: 'unknow', addresses: []}
 }
--- test/index.js
+++ test/index.js
@@ -1,135 +1,361 @@
 import { expect } from 'chai'
-import bitcore from 'bitcore'
+import crypto from 'crypto'
+import bitcoin from 'bitcoinjs-lib'
+import bs58check from 'bs58check'
 
 import script2addresses from '../src'
 
-let Address = bitcore.Address
-let sha256ripemd160 = bitcore.crypto.Hash.sha256ripemd160
+/**
+ * @param {string} s
+ * @return {string}
+ */
+function asm2hex (s) {
+  return bitcoin.script.fromASM(s).toString('hex')
+}
+
+/**
+ * @param {number} [version=0x03]
+ * @param {number} [length=32]
+ * @return {string}
+ */
+function makeRandom (version = 0x03, length = 32) {
+  // probably can return invalid key
+  let pk = Buffer.concat([new Buffer([version]), crypto.randomBytes(length)])
+  return pk.toString('hex')
+}
+
+/**
+ * @param {string} pkHex
+ * @return {string}
+ */
+function makeHash (pkHex) {
+  return bitcoin.crypto.hash160(new Buffer(pkHex, 'hex')).toString('hex')
+}
+
+/**
+ * @param {string} hash
+ * @return {string}
+ */
+function makeP2PKHAddress (hash) {
+  return bs58check.encode(Buffer.concat([
+    new Buffer([bitcoin.networks.bitcoin.pubKeyHash]), new Buffer(hash, 'hex')]))
+}
+
+/**
+ * @param {string} hash
+ * @return {string}
+ */
+function makeP2SHAddress (hash) {
+  return bs58check.encode(Buffer.concat([
+    new Buffer([bitcoin.networks.bitcoin.scriptHash]), new Buffer(hash, 'hex')]))
+}
 
 describe('script2addresses', () => {
-  let pk = bitcore.PrivateKey.fromRandom().toPublicKey()
-  let pkHex = pk.toString()
-  let pkHash = sha256ripemd160(pk.toBuffer()).toString('hex')
-  let pkhAddress = new Address(pk, null, Address.PayToPublicKeyHash)
-  let pshAddress = Address.fromScriptHash(new Buffer(pkHash, 'hex'))
+  describe('arguments', () => {
+    it('not a buffer and neither a string', () => {
+      expect(script2addresses()).to.deep.equal({type: 'unknow', addresses: []})
+    })
+
+    it('apply to buffer', () => {
+      let pkHex = makeRandom()
+      let script = asm2hex(`${pkHex} OP_CHECKSIG`)
+      expect(script2addresses(new Buffer(script, 'hex'), 'mainnet')).to.deep.equal({
+        type: 'pubkey',
+        addresses: [makeP2PKHAddress(makeHash(pkHex))]
+      })
+    })
+
+    it('network as string', () => {
+      let pkHex = makeRandom()
+      let script = asm2hex(`${pkHex} OP_CHECKSIG`)
+      expect(script2addresses(script, 'mainnet')).to.deep.equal({
+        type: 'pubkey',
+        addresses: [makeP2PKHAddress(makeHash(pkHex))]
+      })
+    })
+  })
+
+  describe('isPublicKey', () => {
+    // TODO
+  })
 
   describe('pubkeyhash', () => {
-    it('OP_DUP OP_HASH 20 0x{data} OP_EQUALVERIFY OP_CHECKSIG', () => {
-      let script = `OP_DUP OP_HASH160 20 0x${pkHash} OP_EQUALVERIFY OP_CHECKSIG`
+    it('OP_DUP OP_HASH160 {data} OP_EQUALVERIFY OP_CHECKSIG', () => {
+      let pkHex = makeRandom()
+      let script = asm2hex(`OP_DUP OP_HASH160 ${makeHash(pkHex)} OP_EQUALVERIFY OP_CHECKSIG`)
       expect(script2addresses(script)).to.deep.equal({
         type: 'pubkeyhash',
-        addresses: [pkhAddress.toString()]
+        addresses: [makeP2PKHAddress(makeHash(pkHex))]
       })
     })
 
-    it('OP_DUP OP_HASH 20 0x{data} OP_EQUALVERIFY OP_CHECKSIG (strict)', () => {
-      let script = `OP_DUP OP_HASH160 20 0x${pkHash} OP_EQUALVERIFY OP_CHECKSIG`
+    it('OP_DUP OP_HASH160 {data} OP_EQUALVERIFY OP_CHECKSIG (strict)', () => {
+      let pkHex = makeRandom()
+      let script = asm2hex(`OP_DUP OP_HASH160 ${makeHash(pkHex)} OP_EQUALVERIFY OP_CHECKSIG`)
       expect(script2addresses(script, null, true)).to.deep.equal({
         type: 'pubkeyhash',
-        addresses: [pkhAddress.toString()]
+        addresses: [makeP2PKHAddress(makeHash(pkHex))]
       })
     })
 
-    it('OP_DUP OP_HASH160 OP_PUSHDATA1 20 0x{data} OP_EQUALVERIFY OP_CHECKSIG', () => {
-      let script = `OP_DUP OP_HASH160 OP_PUSHDATA1 20 0x${pkHash} OP_EQUALVERIFY OP_CHECKSIG`
+    it('OP_DUP OP_HASH160 OP_PUSHDATA1 {data} OP_EQUALVERIFY OP_CHECKSIG', () => {
+      let pkHex = makeRandom()
+      let script = asm2hex(`OP_DUP OP_HASH160 OP_PUSHDATA1 ${makeHash(pkHex)} OP_EQUALVERIFY OP_CHECKSIG`)
       expect(script2addresses(script)).to.deep.equal({
         type: 'pubkeyhash',
-        addresses: [pkhAddress.toString()]
+        addresses: [makeP2PKHAddress(makeHash(pkHex))]
       })
     })
 
-    it('OP_DUP OP_HASH160 OP_PUSHDATA1 20 0x{data} OP_EQUALVERIFY OP_CHECKSIG (strict)', () => {
-      let script = `OP_DUP OP_HASH160 OP_PUSHDATA1 20 0x${pkHash} OP_EQUALVERIFY OP_CHECKSIG`
-      expect(script2addresses(script, null, true)).to.deep.equal({type: 'unknow'})
+    it('OP_DUP OP_HASH160 OP_PUSHDATA1 {data (19 bytes)} OP_EQUALVERIFY OP_CHECKSIG', () => {
+      let script = asm2hex(`OP_DUP OP_HASH160 OP_PUSHDATA1 ${crypto.randomBytes(19).toString('hex')} OP_EQUALVERIFY OP_CHECKSIG`)
+      expect(script2addresses(script)).to.deep.equal({
+        type: 'unknow',
+        addresses: []
+      })
+    })
+
+    it('OP_DUP OP_HASH160 OP_PUSHDATA1 {data} OP_EQUALVERIFY OP_CHECKSIG (strict)', () => {
+      let pkHex = makeRandom()
+      let script = asm2hex(`OP_DUP OP_HASH160 OP_PUSHDATA1 ${makeHash(pkHex)} OP_EQUALVERIFY OP_CHECKSIG`)
+      expect(script2addresses(script, null, true)).to.deep.equal({
+        type: 'unknow',
+        addresses: []
+      })
+    })
+
+    it('OP_DUP OP_HASH160 OP_PUSHDATA2 {data} OP_EQUALVERIFY OP_CHECKSIG', () => {
+      let pkHex = makeRandom()
+      let script = asm2hex(`OP_DUP OP_HASH160 OP_PUSHDATA2 ${makeHash(pkHex)} OP_EQUALVERIFY OP_CHECKSIG`)
+      script = script.slice(0, 8) + '00' + script.slice(8)
+      expect(script2addresses(script)).to.deep.equal({
+        type: 'pubkeyhash',
+        addresses: [makeP2PKHAddress(makeHash(pkHex))]
+      })
+    })
+
+    it('OP_DUP OP_HASH160 OP_PUSHDATA4 {data} OP_EQUALVERIFY OP_CHECKSIG', () => {
+      let pkHex = makeRandom()
+      let script = asm2hex(`OP_DUP OP_HASH160 OP_PUSHDATA4 ${makeHash(pkHex)} OP_EQUALVERIFY OP_CHECKSIG`)
+      script = script.slice(0, 8) + '000000' + script.slice(8)
+      expect(script2addresses(script)).to.deep.equal({
+        type: 'pubkeyhash',
+        addresses: [makeP2PKHAddress(makeHash(pkHex))]
+      })
     })
   })
 
   describe('scripthash', () => {
-    it(`OP_HASH160 20 0x{data} OP_EQUAL`, () => {
-      let script = `OP_HASH160 20 0x${pkHash} OP_EQUAL`
+    it('OP_HASH160 {data} OP_EQUAL', () => {
+      let pkHex = makeRandom()
+      let script = asm2hex(`OP_HASH160 ${makeHash(pkHex)} OP_EQUAL`)
       expect(script2addresses(script)).to.deep.equal({
         type: 'scripthash',
-        addresses: [pshAddress.toString()]
+        addresses: [makeP2SHAddress(makeHash(pkHex))]
       })
     })
 
-    it(`OP_HASH160 20 0x{data} OP_EQUAL (strict)`, () => {
-      let script = `OP_HASH160 20 0x${pkHash} OP_EQUAL`
+    it('OP_HASH160 {data} OP_EQUAL (strict)', () => {
+      let pkHex = makeRandom()
+      let script = asm2hex(`OP_HASH160 ${makeHash(pkHex)} OP_EQUAL`)
       expect(script2addresses(script, null, true)).to.deep.equal({
         type: 'scripthash',
-        addresses: [pshAddress.toString()]
+        addresses: [makeP2SHAddress(makeHash(pkHex))]
       })
     })
 
-    it(`OP_HASH160 OP_PUSHDATA1 20 0x{data} OP_EQUAL`, () => {
-      let script = `OP_HASH160 OP_PUSHDATA1 20 0x${pkHash} OP_EQUAL`
+    it('OP_HASH160 OP_PUSHDATA1 {data} OP_EQUAL', () => {
+      let pkHex = makeRandom()
+      let script = asm2hex(`OP_HASH160 OP_PUSHDATA1 ${makeHash(pkHex)} OP_EQUAL`)
       expect(script2addresses(script)).to.deep.equal({
         type: 'scripthash',
-        addresses: [pshAddress.toString()]
+        addresses: [makeP2SHAddress(makeHash(pkHex))]
       })
     })
 
-    it(`OP_HASH160 OP_PUSHDATA1 20 0x{data} OP_EQUAL (strict)`, () => {
-      let script = `OP_HASH160 OP_PUSHDATA1 20 0x${pkHash} OP_EQUAL`
-      expect(script2addresses(script, null, true)).to.deep.equal({type: 'unknow'})
+    it('OP_HASH160 OP_PUSHDATA1 {data (19 bytes)} OP_EQUAL', () => {
+      let script = asm2hex(`OP_HASH160 OP_PUSHDATA1 ${crypto.randomBytes(19).toString('hex')} OP_EQUAL`)
+      expect(script2addresses(script)).to.deep.equal({
+        type: 'unknow',
+        addresses: []
+      })
+    })
+
+    it('OP_HASH160 OP_PUSHDATA1 {data} OP_EQUAL (strict)', () => {
+      let pkHex = makeRandom()
+      let script = asm2hex(`OP_HASH160 OP_PUSHDATA1 ${makeHash(pkHex)} OP_EQUAL`)
+      expect(script2addresses(script, null, true)).to.deep.equal({
+        type: 'unknow',
+        addresses: []
+      })
+    })
+
+    it('OP_HASH160 OP_PUSHDATA2 {data} OP_EQUAL', () => {
+      let pkHex = makeRandom()
+      let script = asm2hex(`OP_HASH160 OP_PUSHDATA2 ${makeHash(pkHex)} OP_EQUAL`)
+      script = script.slice(0, 6) + '00' + script.slice(6)
+      expect(script2addresses(script)).to.deep.equal({
+        type: 'scripthash',
+        addresses: [makeP2SHAddress(makeHash(pkHex))]
+      })
+    })
+
+    it('OP_HASH160 OP_PUSHDATA4 {data} OP_EQUAL', () => {
+      let pkHex = makeRandom()
+      let script = asm2hex(`OP_HASH160 OP_PUSHDATA4 ${makeHash(pkHex)} OP_EQUAL`)
+      script = script.slice(0, 6) + '000000' + script.slice(6)
+      expect(script2addresses(script)).to.deep.equal({
+        type: 'scripthash',
+        addresses: [makeP2SHAddress(makeHash(pkHex))]
+      })
     })
   })
 
   describe('nulldata', () => {
     it('OP_RETURN', () => {
-      let script = `OP_RETURN`
-      expect(script2addresses(script)).to.deep.equal({type: 'nulldata'})
+      let script = asm2hex(`OP_RETURN`)
+      expect(script2addresses(script)).to.deep.equal({type: 'nulldata', addresses: []})
+    })
+
+    it('OP_RETURN 48656c6c6f20776f726c6421', () => {
+      let script = asm2hex(`OP_RETURN 48656c6c6f20776f726c6421`)
+      expect(script2addresses(script)).to.deep.equal({type: 'nulldata', addresses: []})
     })
   })
 
   describe('pubkey', () => {
-    it('33 0x{data} OP_CHECKSIG', () => {
-      let script = `33 0x${pkHex} OP_CHECKSIG`
+    it('{data (33 bytes)} OP_CHECKSIG', () => {
+      let pkHex = makeRandom(0x03, 32)
+      let script = asm2hex(`${pkHex} OP_CHECKSIG`)
       expect(script2addresses(script)).to.deep.equal({
         type: 'pubkey',
-        addresses: [pkhAddress.toString()]
+        addresses: [makeP2PKHAddress(makeHash(pkHex))]
       })
     })
 
-    it('33 0x{data} OP_CHECKSIG (strict)', () => {
-      let script = `33 0x${pkHex} OP_CHECKSIG`
+    it('{data (65 bytes)} OP_CHECKSIG', () => {
+      let pkHex = makeRandom(0x04, 64)
+      let script = asm2hex(`${pkHex} OP_CHECKSIG`)
+      expect(script2addresses(script)).to.deep.equal({
+        type: 'pubkey',
+        addresses: [makeP2PKHAddress(makeHash(pkHex))]
+      })
+    })
+
+    it('OP_PUSHDATA1 {data} OP_CHECKSIG', () => {
+      let pkHex = makeRandom()
+      let script = asm2hex(`OP_PUSHDATA1 ${pkHex} OP_CHECKSIG`)
+      expect(script2addresses(script)).to.deep.equal({
+        type: 'pubkey',
+        addresses: [makeP2PKHAddress(makeHash(pkHex))]
+      })
+    })
+
+    it('OP_PUSHDATA1 {data (32 bytes)} OP_CHECKSIG', () => {
+      let script = asm2hex(`OP_PUSHDATA1 ${crypto.randomBytes(32).toString('hex')} OP_CHECKSIG`)
+      expect(script2addresses(script)).to.deep.equal({
+        type: 'unknow',
+        addresses: []
+      })
+    })
+
+    it('OP_PUSHDATA1 {data} OP_CHECKSIG (strict)', () => {
+      let pkHex = makeRandom()
+      let script = asm2hex(`OP_PUSHDATA1 ${pkHex} OP_CHECKSIG`)
       expect(script2addresses(script, null, true)).to.deep.equal({
-        type: 'pubkey',
-        addresses: [pkhAddress.toString()]
+        type: 'unknow',
+        addresses: []
       })
     })
 
-    it('OP_PUSHDATA1 33 0x{data} OP_CHECKSIG', () => {
-      let script = `OP_PUSHDATA1 33 0x${pkHex} OP_CHECKSIG`
+    it('OP_PUSHDATA2 {data} OP_CHECKSIG', () => {
+      let pkHex = makeRandom()
+      let script = asm2hex(`OP_PUSHDATA2 ${pkHex} OP_CHECKSIG`)
+      script = script.slice(0, 4) + '00' + script.slice(4)
       expect(script2addresses(script)).to.deep.equal({
         type: 'pubkey',
-        addresses: [pkhAddress.toString()]
+        addresses: [makeP2PKHAddress(makeHash(pkHex))]
       })
     })
 
-    it('OP_PUSHDATA1 33 0x{data} OP_CHECKSIG (strict)', () => {
-      let script = `OP_PUSHDATA1 33 0x${pkHex} OP_CHECKSIG`
-      expect(script2addresses(script, null, true)).to.deep.equal({type: 'unknow'})
+    it('OP_PUSHDATA4 {data} OP_CHECKSIG', () => {
+      let pkHex = makeRandom()
+      let script = asm2hex(`OP_PUSHDATA4 ${pkHex} OP_CHECKSIG`)
+      script = script.slice(0, 4) + '000000' + script.slice(4)
+      expect(script2addresses(script)).to.deep.equal({
+        type: 'pubkey',
+        addresses: [makeP2PKHAddress(makeHash(pkHex))]
+      })
     })
   })
 
   describe('multisig', () => {
-    it('OP_2 33 0x{data} 33 0x{data} OP_2 OP_CHECKMULTISIG', () => {
-      let script = `OP_2 33 0x${pkHex} 33 0x${pkHex} OP_2 OP_CHECKMULTISIG`
+    it('OP_2 {data} {data} OP_2 OP_CHECKMULTISIG', () => {
+      let pkHex = makeRandom()
+      let script = asm2hex(`OP_2 ${pkHex} ${pkHex} OP_2 OP_CHECKMULTISIG`)
       expect(script2addresses(script)).to.deep.equal({
         type: 'multisig',
-        addresses: [pkhAddress.toString()]
+        addresses: [makeP2PKHAddress(makeHash(pkHex))]
       })
     })
 
-    it('OP_1 33 0x{data} OP_2 OP_CHECKMULTISIG', () => {
-      let script = `OP_1 33 0x${pkHex} OP_2 OP_CHECKMULTISIG`
-      expect(script2addresses(script)).to.deep.equal({type: 'unknow'})
+    it('OP_2 {data1} {data2} OP_2 OP_CHECKMULTISIG', () => {
+      let pkHex1 = makeRandom()
+      let pkHex2 = makeRandom()
+      let script = asm2hex(`OP_2 ${pkHex1} ${pkHex2} OP_2 OP_CHECKMULTISIG`)
+      let result = script2addresses(script)
+      expect(result).to.have.property('type', 'multisig')
+      expect(result).to.have.property('addresses').to.be.an('Array')
+      expect(result.addresses.sort()).to.deep.equal([
+        makeP2PKHAddress(makeHash(pkHex1)),
+        makeP2PKHAddress(makeHash(pkHex2))
+      ].sort())
     })
 
-    it('OP_1 65 0x{data} OP_1 OP_CHECKMULTISIG (strict)', () => {
-      let script = `OP_1 65 0x06${new Buffer(64).toString('hex')} OP_1 OP_CHECKMULTISIG`
-      expect(script2addresses(script, null, true)).to.deep.equal({type: 'unknow'})
+    it('OP_1 OP_PUSHDATA1 {data} OP_1 OP_CHECKMULTISIG', () => {
+      let pkHex = makeRandom()
+      let script = asm2hex(`OP_1 OP_PUSHDATA1 ${pkHex} OP_1 OP_CHECKMULTISIG`)
+      expect(script2addresses(script)).to.deep.equal({
+        type: 'multisig',
+        addresses: [makeP2PKHAddress(makeHash(pkHex))]
+      })
+    })
+
+    it('OP_1 OP_PUSHDATA1 {data (32 bytes)} OP_1 OP_CHECKMULTISIG', () => {
+      let script = asm2hex(`OP_1 OP_PUSHDATA1 ${crypto.randomBytes(32).toString('hex')} OP_1 OP_CHECKMULTISIG`)
+      expect(script2addresses(script)).to.deep.equal({
+        type: 'unknow',
+        addresses: []
+      })
+    })
+
+    it('OP_1 OP_PUSHDATA1 {data} OP_1 OP_CHECKMULTISIG (strict)', () => {
+      let pkHex = makeRandom()
+      let script = asm2hex(`OP_1 OP_PUSHDATA1 ${pkHex} OP_1 OP_CHECKMULTISIG`)
+      expect(script2addresses(script, null, true)).to.deep.equal({
+        type: 'unknow',
+        addresses: []
+      })
+    })
+
+    it('OP_1 OP_PUSHDATA2 {data} OP_1 OP_CHECKMULTISIG', () => {
+      let pkHex = makeRandom()
+      let script = asm2hex(`OP_1 OP_PUSHDATA2 ${pkHex} OP_1 OP_CHECKMULTISIG`)
+      script = script.slice(0, 6) + '00' + script.slice(6)
+      expect(script2addresses(script)).to.deep.equal({
+        type: 'multisig',
+        addresses: [makeP2PKHAddress(makeHash(pkHex))]
+      })
+    })
+
+    it('OP_1 OP_PUSHDATA4 {data} OP_1 OP_CHECKMULTISIG', () => {
+      let pkHex = makeRandom()
+      let script = asm2hex(`OP_1 OP_PUSHDATA4 ${pkHex} OP_1 OP_CHECKMULTISIG`)
+      script = script.slice(0, 6) + '000000' + script.slice(6)
+      expect(script2addresses(script)).to.deep.equal({
+        type: 'multisig',
+        addresses: [makeP2PKHAddress(makeHash(pkHex))]
+      })
     })
   })
 })
